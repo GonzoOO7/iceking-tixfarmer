dbd33e6026beaaea15dd2946a6e72b8baebfd103
GonzoOO7 <gonzo007.mc@gmail.com>
2026-05-09 22:07:12 +0200

    hehe turst

---
+++ README.md
@@ -0,0 +1,37 @@
+# Tix farming :)
+
+## Setup program
+
+get python if you dont have it
+
+install keyboard package im too lazy to write how
+
+get this repo (its just one script really)
+
+## Setup ingame
+
+takes these cards:
+
+agressor, calypso, knight, and as many ivestors as possible
+
+![cards.png](cards.png)
+
+btw you need 18sp and 7hp minimum
+
+you could also take sp saver and need only 14 sp or smth if you really want to...
+
+## How to start
+
+go to the guru and say you very cold :freezy:
+
+walk to the king but DO NOT PRESS E
+
+start the script, you'll have 3 seconds to switch to your game
+
+!important (yes this is css :) you MUST NOT HAVE YOUR MOUSE NEAR YOUR ACTIONS
+
+if you have, just put it on your second screen after focusing the game
+
+## Result
+
+you can now do anything else and be happy ^-^
---
+++ iceking_tixfarmer.py
@@ -0,0 +1,73 @@
+# Make sure to stand at the ice king by the guru
+import time
+import keyboard
+
+TIMES = 3
+
+def info(text):
+    print( "\033[3;34m" + text + "\033[m")
+
+
+def action(text):
+    print( "\033[1;36m" + text + "\033[m")
+
+def click(key):
+    keyboard.press_and_release(key)
+
+def timeout(x):
+    time.sleep(x)
+
+def enter():
+    action("Enter fight")
+    click('e')
+    timeout(6)
+
+def call():
+    action("Call calypso")
+    for i in ['d', 's', 'space', 's', 'space']:
+        click(i)
+    timeout(3)
+
+def paint():
+    action("Paint king")
+    for i in ['d', 'space', 'd', 'd', 'd', 'd', 'space', 'space']:
+        click(i)
+    timeout(4)
+
+def await_calypso_king():
+    info("Await calypso")
+    timeout(3)
+    info("Await king")
+    timeout(3)
+
+def stabby_stab():
+    for i in range(2):
+        action(f"Stab {i+1}x")
+        for j in ['space', 'd', 'space']:
+            click(j)
+        timeout(5)
+
+def await_calypso_win():
+    info("Await calypso")
+    timeout(6)
+    info("Await victory")
+    timeout(9)
+
+def run_it_all():
+    enter()
+    call()
+    paint()
+    await_calypso_king()
+    stabby_stab()
+    await_calypso_win()
+
+action(f"Ready for {TIMES} runs")
+timeout(3)
+for r in range(TIMES):
+    run_it_all()
+    if r == TIMES - 1:
+        info(f"All done :) ({TIMES} times)")
+        break
+    else:
+        info(f"Here we go again x{r+1}")
+
